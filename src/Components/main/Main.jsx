b17a2abdffd014da8b08ea90640a5a57b56eaefc
PlinioGS <alliothsilva61@gmail.com>
2025-04-07 21:59:09 -0300

    Corrigindo erros

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -2,7 +2,7 @@ import card1 from '../../assets/card1.png'
 import card2 from '../../assets/card2.png'
 import card3 from '../../assets/card3.png'
 import card4 from '../../assets/card4.png'
-import s from './Main.module.scss'
+import s from './main.module.scss'
 
 export default function Main(){
     return(
